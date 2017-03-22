f3b591fe88eccb97b48ead2a2e591602b29fcdf3
lakhminder <lakhminder@gmail.com>
2017-03-22 14:14:19 +0530

    sorting 'rejected non-fast-forward' error

--- TestJavaProject/src/Test.java
+++ TestJavaProject/src/Test.java
@@ -1,10 +1,18 @@
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+import org.generics.TestGeneric;
+
 
 
 public class Test {
-
+private int count = 0;
+	static{
+		System.out.println("Static block!!");
+	}
 	/**
 	 * @param args
 	 */
@@ -20,8 +28,12 @@ public class Test {
 		
 	}*/
 	
+	/*public void test(){
+		System.out.println("Inside constructor");
+	}*/
+	
 	public static void main(String[] args) {
-		int rowCount = 5;
+		/*int rowCount = 5;		
 		Date date = new Date(2014, 3, 5, 0, 52);
 		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
 		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -29,7 +41,75 @@ public class Test {
 		System.out.println("Time value using kk:" + sdf1.format(date));
 		System.out.println("Time Value using HH:" + sdf2.format(date));
 		System.out.println(rowCount);
+		Parent parent = new Child();
+		parent.display();*/
+		
+		TestGeneric test = new TestGeneric(); 
+		List<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(789);
+		test.printList(a);
+		List<Float> b = new ArrayList();
+		b.add(5F);
+		test.printList(b);
+	}
+	public Test() {
+		System.out.println("Constructor");
 	}
 		
 
 }
+
+ class Parent{
+	 static{
+		System.out.println("Parent static block"); 
+	 }
+	 
+	
+	 public Parent(){
+		 System.out.println("Parent constructor");
+	 }
+	public Parent display(){
+		System.out.println("In Parent");
+		return null;
+	}
+}
+ 
+ class TestException{
+	 int i = 0;
+	 void show(){
+		 if (i == 0)
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			}
+	 }
+ }
+ 
+ class Child extends Parent {
+	 static{
+			System.out.println("Child static block"); 
+		 }
+	 //@Override
+	 public Parent display(){
+	 //public Child display(){
+		 System.out.println("Child Display");
+		 return null;	 
+		
+	 }
+	 
+	 public void show(){
+		 System.out.println("Inside Show");
+	 }
+	 
+	 Child(){
+		 System.out.println("Child Constructor");
+		 
+	 }	 
+	 
+ }
+ 
+ 
