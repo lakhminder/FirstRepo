90348d75b59a8c715ced35912466e5ea4be2c750
lakhminder <lakhminder@gmail.com>
2017-04-08 16:53:45 +0530

    consumer with Future

--- TestJavaProject/src/org/lucky/producerConsumer/Main.java
+++ TestJavaProject/src/org/lucky/producerConsumer/Main.java
@@ -14,40 +14,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//BlockingQueue<? super Serializable> queue = new ArrayBlockingQueue<String>(5); // not working generics
 		BlockingQueue<String> queue = new ArrayBlockingQueue<String>(5);
 		
 		Thread singleProducer = new Thread(new Producer(queue));
-		
-		//BlockingQueue<Runnable> threadExecutorQueue = new ArrayBlockingQueue<Runnable>(1);
-		
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 2, 3, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(1), new ThreadFactory() {
-			int counter = 0;
-			@Override
-			public Thread newThread(Runnable r) {
-				// TODO Auto-generated method stub
-				return new Thread(r,"ConsumerThread" + counter++);
-			}
-		}, new RejectedExecutionHandler() {
-			
-			@Override
-			public void rejectedExecution(Runnable r, ThreadPoolExecutor poolExecutor) {
-				try {
-					System.out.println("Rejected:" + r);
-					poolExecutor.getQueue().put(r);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-		}) ;
+		Thread consumer = new Thread(new Consumer(queue));		
 		
 		singleProducer.start();
-		executor.execute(new Consumer(queue));
-		executor.execute(new Consumer(queue));
-		executor.shutdown();
-		//Executor consumer = Executors.newFixedThreadPool
+		consumer.start();
+		
 	}
 
 }
